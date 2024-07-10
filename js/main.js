f7c84dba31243b38118689fa6929643bc28c92bb
towfiqislambd <towfiqislambd@gmail.com>
2024-07-10 12:52:39 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.html').rProgressbar({
+    percentage: 90,
+    duration: 2000,
+    fillBackgroundColor:'#754ef9',
+    backgroundColor:'#ddd',
+    borderRadius:'6px',
+    height:'5px',
+    width:'100%'
+  });
+  $('.css').rProgressbar({
+    percentage: 60,
+    duration: 2000,
+    fillBackgroundColor:'#754ef9',
+    backgroundColor:'#ddd',
+    borderRadius:'6px',
+    height:'5px',
+    width:'100%'
+  });
+  $('.js').rProgressbar({
+    percentage: 70,
+    duration: 2000,
+    fillBackgroundColor:'#754ef9',
+    backgroundColor:'#ddd',
+    borderRadius:'6px',
+    height:'5px',
+    width:'100%'
+  });
+  $('.jq').rProgressbar({
+    percentage: 80,
+    duration: 2000,
+    fillBackgroundColor:'#754ef9',
+    backgroundColor:'#ddd',
+    borderRadius:'6px',
+    height:'5px',
+    width:'100%'
+  })
+})
